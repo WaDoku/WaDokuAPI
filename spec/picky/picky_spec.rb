d1ba79c87f953f891545e12046339b429483a479
Roger Braun <roger@rogerbraun.net>
2013-02-17 17:06:18 +0100

    Specs for Picky.

---
+++ spec/picky/picky_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe WadokuSearch do
+
+  describe "sorting" do
+    it 'should favor hits in tres' do
+      entries = [
+        Entry.get(112), # 相… [1]
+        Entry.get(631), # 青
+        Entry.get(9999) # 日本 [a]; ニッポン
+      ]
+      Picky::Indexes.each do |index|
+        index.clear
+        entries.each do |entry|
+          index.replace entry
+        end
+      end
+      res = WadokuSearch.search("japan", 30, 0)
+      res.ids[0].should == 9999
+    end
+  end
+
+end
