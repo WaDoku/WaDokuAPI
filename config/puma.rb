66227ca0bf65141af43c9604afd6747455d2b006
Toni Förster <toni.foerster@gmail.com>
2018-05-06 22:47:03 +0200

    added puma config options

--- config/puma.rb
+++ config/puma.rb
@@ -3,3 +3,7 @@ environment 'staging'
 bind 'tcp://127.0.0.1:10012'
 workers 4
 preload_app!
+
+state_path '#{ROOT_DIR}/tmp/pids/puma.state'
+pidfile '#{ROOT_DIR}/tmp/pids/puma.pid'
+activate_control_app 'unix:///var/run/wadoku_api_staging_pumactl.sock'
