bd852d4ecd3f459fad215cb9d673969676b037e3
Toni Förster <toni.foerster@gmail.com>
2018-05-06 23:24:03 +0200

    make path absolute

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,6 @@ bind 'tcp://127.0.0.1:10012'
 workers 4
 preload_app!
 
-state_path '#{ROOT_DIR}/tmp/pids/puma.state'
-pidfile '#{ROOT_DIR}/tmp/pids/puma.pid'
+state_path '/var/www/staging/wadoku_api/current/tmp/pids/puma.state'
+pidfile '/var/www/staging/wadoku_api/current/tmp/pids/puma.pid'
 activate_control_app 'unix:///var/run/wadoku_api_staging_pumactl.sock'
