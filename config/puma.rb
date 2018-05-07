d7a237e0aa22c06a9c2072317cc295745910ed0e
Toni Förster <toni.foerster@gmail.com>
2018-05-07 02:35:15 +0200

    puma config changes

--- config/puma.rb
+++ config/puma.rb
@@ -4,5 +4,6 @@ bind 'tcp://127.0.0.1:10012'
 workers 4
 preload_app!
 
-state_path '/var/www/staging/wadoku_api/current/tmp/pids/puma.state'
-pidfile '/var/www/staging/wadoku_api/current/tmp/pids/puma.pid'
+state_path File.expand_path('../', File.dirname(__FILE__))+'/tmp/puma.state'
+pidfile File.expand_path('../', File.dirname(__FILE__))+'/tmp/pids/puma'
+activate_control_app 'unix://'+File.expand_path('../', File.dirname(__FILE__))+'/tmp/pumactl.sock'
